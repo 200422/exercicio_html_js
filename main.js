8fa538630c37721ed25abe0177e3f53f2ea4daeb
joão <monteirosousaaa20@gmail.com>
2022-12-10 11:00:02 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.getElementsByClassName('forms');
+
+
+
+
+const validar = function(valor){
+    const validadoArray = parserInt(valor)
+    return messageValid
+
+}
+
+
+form.addEventListener('submit', function(e){
+var evento = e.preventDefalt();
+
+})
+
+
+
+
+
+
+
+
+console.log(form)
