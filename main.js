27edfa31b96bf3f812fac73ad8c7f97bc7f1f2ad
joão <monteirosousaaa20@gmail.com>
2022-12-17 12:01:51 -0300

    condições

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 const form = document.getElementById('forms')
 
 form.addEventListener('submit', function(e) {
-    e.preventdefalt();
+    var nValido = false
+    e.preventDefault();
 
-    let numberOne = document.getElementById('firstNumber')
-    let numberTwo = document.getElementById('secondNumber')
+    let numberOne = document.getElementById('firstNumber');
+    let numberTwo = document.getElementById('secondNumber');
+    let successMessage = `O número ${numberTwo} é maior que ${numberOne}`
 
-    if (numberTwo . numberOne) {
-        alert('vitoria') 
-    } else {
-        alert('derrota')
+    const valid = nValido
+    if (numberTwo > numberOne) {
+        
+        const containerSuccessMessage = document.getElementById('message').innerHTML = successMessage;
+        document.getElementById('message').innerHTML = successMessage;
+
+
+    } else{
+        alert(` O numero ${numberTwo} não é maior que ${numberOne} `);
     }
+
 })
-
-
-
-
-
-
 
 
 
